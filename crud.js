8184198e718d39b3427f24ddb1b63cb61123647e
Rishabh Jain <rishabh.jain@heliostechservices.com>
2024-02-26 12:58:35 +0530

    perform opertions on files

---
+++ crud.js
@@ -0,0 +1,16 @@
+const fs=require('fs');
+////Create a folder
+// fs.mkdirSync('node folder');
+////Create a file inside the folder
+// fs.writeFileSync('C:\\Users\\happy\\node js github\\node folder\\test.txt','This is a test file creation');
+////Create append the content in existing file inside the folder
+// fs.appendFileSync('C:\\Users\\happy\\node js github\\node folder\\test.txt','\nAppending more data')
+////Create read the content in file
+// let data=fs.readFileSync('C:\\Users\\happy\\node js github\\node folder\\test.txt',{encoding:'utf-8'})
+// console.log(data)
+////Create rename the file
+// fs.renameSync('C:\\Users\\happy\\node js github\\node folder\\test.txt','myBio.txt')
+////Create delete the file
+// fs.unlinkSync('C:\\Users\\happy\\node js github\\readwrite.txt');
+////Create delete the folder
+// fs.rmdirSync('C:\\Users\\happy\\node js github\\node folder')
