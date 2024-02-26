53b13c612f2dd8d76ffa58379ea2790195e6ab3b
Rishabh Jain <rishabh.jain@heliostechservices.com>
2024-02-26 13:31:41 +0530

    async challenge

--- fsAsync/index.js
+++ fsAsync/index.js
@@ -7,10 +7,37 @@ const fs=require('fs');
 //     console.log('Data is appended')
 // })
 
-fs.readFile('read.txt',{encoding:'utf-8'},(err,data)=>{
-    if(data){
-        console.log(data)
-    }else{
-        console.log(err)
-    }
-})
+// fs.readFile('read.txt',{encoding:'utf-8'},(err,data)=>{
+//     if(data){
+//         console.log(data)
+//     }else{
+//         console.log(err)
+//     }
+// })
+
+// fs.mkdir('node folder',(err,data)=>{
+//     console.log('Folder created')
+// })
+
+// fs.writeFile('node folder/test.txt','This is async file creation',(err,data)=>{
+//     console.log('file is created ')
+// })
+// fs.appendFile('node folder/test.txt','Adding more content',(err,data)=>{
+//     console.log('content is added ')
+// })
+
+// fs.readFile('node folder/test.txt','utf-8',(err,data)=>{
+//     console.log(data)
+// })
+
+// fs.rename('node folder/test.txt','node folder/myBio.txt',(err)=>{
+//     console.log('Rename the file');
+// });
+
+// fs.unlink('node folder/myBio.txt',(err)=>{
+//     console.log('File is deleted')
+// })
+
+// fs.rmdir('node folder',(err)=>{
+//     console.log('Folder is deleted')
+// })
