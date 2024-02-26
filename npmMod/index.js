4d63b270738b212aa828c0f4e9c71d809ec1ee37
Rishabh Jain <rishabh.jain@heliostechservices.com>
2024-02-26 16:25:23 +0530

    use npm

---
+++ npmMod/index.js
@@ -0,0 +1,10 @@
+// const chalk = require('chalk');
+// console.log(chalk.blue('Hello world!')); 
+// const error = chalk.bold.red;
+// const warning = chalk.hex('#FFA500'); // Orange color
+
+// console.log(error('Error!'));
+// console.log(warning('Warning!'));
+
+// const validator = require('validator')
+// console.log(validator.isEmail('rishabh@gmail.com'));
