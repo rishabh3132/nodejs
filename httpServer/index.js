028ee619e0b363dff874e0f1572075b30467b3d1
Rishabh Jain <rishabh.jain@heliostechservices.com>
2024-02-27 11:17:37 +0530

    add error

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,6 +1,16 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
-    res.end('Hello From Other Side')
+    console.log(req.url);
+    if(req.url=='/'){
+        res.end('Home Page')
+    }else if(req.url=='/about'){
+        res.end('About Page')
+    }else if(req.url=='/contact'){
+        res.end('Contact Page')
+    }else{
+        res.writeHead(404,{"content-type":"text/html"});
+        res.end('<h1>No Page Found</h1>')
+    }
 })
 server.listen(8000,'127.0.0.1',()=>{
     console.log('Listening to the server 8000');
