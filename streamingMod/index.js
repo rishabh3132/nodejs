1a96e4afa3baeba2054a14f4b80f407ad0925244
Rishabh Jain <rishabh.jain@heliostechservices.com>
2024-02-27 13:42:13 +0530

    add pipe in streaming

--- streamingMod/index.js
+++ streamingMod/index.js
@@ -2,23 +2,28 @@ const fs=require('fs')
 const http=require('http')
 const server=http.createServer();
 server.on('request',(req,res)=>{
+    ////1st way
     // fs.readFile('input.txt',(err,data)=>{
     //     if(err) return console.log(err);
     //     res.end(data.toString());
     // })
 
-    //readable stream
+    // //2nd way - readable stream
+    // const rStream=fs.createReadStream('input.txt');
+    // rStream.on('data',(chunk)=>{
+    //     res.write(chunk)
+    // })
+    // rStream.on('end',()=>{
+    //     res.end()
+    // })
+    // rStream.on('error',(err)=>{
+    //     console.log(err)
+    //     res.end('File not found')
+    // })
+
+    ////3rd way
     const rStream=fs.createReadStream('input.txt');
-    rStream.on('data',(chunk)=>{
-        res.write(chunk)
-    })
-    rStream.on('end',()=>{
-        res.end()
-    })
-    rStream.on('error',(err)=>{
-        console.log(err)
-        res.end('File not found')
-    })
+    rStream.pipe(res);
 
 })
 server.listen(8000,'127.0.0.1');
