c44b7d7af694eabf879b3e463bfd2037c3084f59
Rishabh Jain <rishabh.jain@heliostechservices.com>
2024-02-29 16:32:47 +0530

    fix time stamp

--- timestamp.js
+++ timestamp.js
@@ -1,6 +1,14 @@
-const sunsetTimestamp = 1709083364;
-const sunsetDate = new Date(sunsetTimestamp * 1000);
-const utcSunsetTime = sunsetDate.toUTCString();
-const localSunsetTime = new Date(sunsetDate.getTime() + (5.5 * 60 * 60 * 1000));
-const formattedLocalSunsetTime = localSunsetTime.toLocaleString();
-console.log("Sunset time in Jaipur:", formattedLocalSunsetTime);
+// const sunsetTimestamp = 1709169706;
+// const sunsetDate = new Date(sunsetTimestamp * 1000);
+// const utcSunsetTime = sunsetDate.toUTCString();
+// const localSunsetTime = new Date(sunsetDate.getTime() + (5.5 * 60 * 60 * 1000));
+// const formattedLocalSunsetTime = localSunsetTime.toLocaleString();
+// console.log("Sunset time in Jaipur:", formattedLocalSunsetTime);
+
+const timestamp = 1709169706;
+const utcDate = new Date(timestamp * 1000);
+const indiaOffset = 5.5 * 60 * 60 * 1000;
+const indiaDate = new Date(utcDate.getTime() + indiaOffset);
+const formattedDate = indiaDate.toISOString();
+console.log("UTC Time:", utcDate.toISOString());
+console.log("India Time:", formattedDate);
